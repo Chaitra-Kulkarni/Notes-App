a97cf9456fec8ae3211afe90fc9848a35fa557b9
Chaitra-Kulkarni <rkchaitra22@gmail.com>
2022-04-10 14:33:38 +0530

    Show & Hide Popup Form

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-// 10.35 //
+const addBox = document.querySelector(".add-box");
+const popupBox = document.querySelector(".popup-box");
+const closeIcon = document.querySelector("header i");
+
+// console.log(addBox);
+addBox.addEventListener("click", () => {
+    popupBox.classList.add("show")
+});
+
+closeIcon.addEventListener("click", () => {
+    popupBox.classList.remove("show");
+})
