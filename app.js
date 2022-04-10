12db6ba5135d23ffd027bd465b762c4be63e1ffe
Chaitra-Kulkarni <rkchaitra22@gmail.com>
2022-04-10 14:44:36 +0530

    Add Note button

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 const addBox = document.querySelector(".add-box");
 const popupBox = document.querySelector(".popup-box");
-const closeIcon = document.querySelector("header i");
+const closeIcon = popupBox.querySelector("header i");
+const addButton = popupBox.querySelector("button");
+const titleTag = popupBox.querySelector("input");
+const descTag = popupBox.querySelector("textarea");
 
-// console.log(addBox);
+// Show & Hide Popup Form
 addBox.addEventListener("click", () => {
     popupBox.classList.add("show")
 });
 
 closeIcon.addEventListener("click", () => {
     popupBox.classList.remove("show");
+})
+
+// Add Note button
+addButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    console.log("button clicked");
 })
