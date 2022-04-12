871d3b21db5df159957f04da772ce622584c45ab
Chaitra-Kulkarni <rkchaitra22@gmail.com>
2022-04-12 15:45:39 +0530

    push code

--- app.js
+++ app.js
@@ -16,6 +16,8 @@ addBox.addEventListener("click", () => {
 });
 
 closeIcon.addEventListener("click", () => {
+    titleTag.value = "";
+    descTag.value = "";
     popupBox.classList.remove("show");
 });
 
